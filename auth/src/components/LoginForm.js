3082f875052d3903fc94fd11ecd3c150e28da7c0
RaghdaWesam <raghda.wessam@gmail.com>
2018-01-21 16:02:28 +0200

    login authentication

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = {
-    email: '',
-    password: ''
-  };
+  state =
+  { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    console.log('login press!');
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        console.log('bad req 1');
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            console.log('bad req 2');
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
+
   render() {
     return (
       <Card>
@@ -17,6 +34,7 @@ class LoginForm extends Component {
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
+
         <CardSection>
           <Input
             placeholder="password"
@@ -27,8 +45,12 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={() => { this.onButtonPress(); }}>
             Log in
           </Button>
         </CardSection>
@@ -37,6 +59,14 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center', // center text on screen
+    color: 'red'
+  }
+};
 
 export default LoginForm;
 
