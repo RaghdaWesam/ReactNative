ab24db54db5816b7c782bf807c579a004e2e1702
RaghdaWesam <raghda.wessam@gmail.com>
2018-01-27 17:14:35 +0200

    firebase setup

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCXrdo4bTI9xlgSahxcyfqV6FNgClyWBiI',
+      authDomain: 'manager-e34cf.firebaseapp.com',
+      databaseURL: 'https://manager-e34cf.firebaseio.com',
+      projectId: 'manager-e34cf',
+      storageBucket: '',
+      messagingSenderId: '650642615648'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
