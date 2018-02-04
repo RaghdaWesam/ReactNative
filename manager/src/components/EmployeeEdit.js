e54a943c8e838bba38acdd6117b550a8e6021b8b
RaghdaWesam <raghda.wessam@gmail.com>
2018-02-04 23:06:28 +0200

    delete emp layout

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -4,10 +4,14 @@ import Communications from 'react-native-communications';
 import _ from 'lodash';
 import * as actions from '../actions';
 import EmployeeForm from './EmployeeForm';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 
 class EmployeeEdit extends Component {
+  state = {
+    showModal: false
+
+  }
   componentWillMount() {
     // map on each property in employee object and send it to update method
     // prop --> key {name , phone, shift}
@@ -40,6 +44,16 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
